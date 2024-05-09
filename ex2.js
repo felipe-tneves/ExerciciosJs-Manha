f5e5a05e12894a9cee96a467a779f654c6e1e7a2
felipe-tneves <felipe.tadeusantos1@gmail.com>
2024-05-09 08:45:52 -0300

    feat: exercicio 2

---
+++ ex2.js
@@ -0,0 +1,4 @@
+// Exercício 2: Verificação de dia da semana
+// Escreva um programa que solicita ao usuário um número de 1 a 7 representando um dia da
+// semana e exibe o nome correspondente a esse dia. Considere que 1 representa segunda-feira,
+// 2 representa terça-feira e assim por diante.
