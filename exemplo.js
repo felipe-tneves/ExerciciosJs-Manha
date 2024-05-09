b95c5fb485f01c8f31b1a9c18f28a9f8574e6992
felipe-tneves <felipe.tadeusantos1@gmail.com>
2024-05-09 08:43:05 -0300

    feat: exercicio 01

---
+++ exemplo.js
@@ -0,0 +1,38 @@
+//Apresentar alguma mensagem na tela
+// alert("Hello Word!")
+
+//Apresenta uma mensagem no console
+// console.log("Console: Hello Word!")
+
+//Apresenta uma mensagem e pega alguma informação do usuário
+// prompt("Mensagem na tela que pega alguma informação")
+
+//Pegando um nome e apresentando na tela
+// let nome = prompt("Digite o seu nome: ");
+
+// console.log("Bem vindo! " + nome)
+
+
+//Fazendo conta com javascript
+
+//parseInt => Transforma o que está entre () em número inteiro: 1, 2, 3, ....
+// let nota1 = parseInt(prompt("Digite uma nota: "));
+
+// let nota2 = parseInt(prompt("Digite outra nota: "));
+
+
+// let media = (nota1 + nota2) /2
+
+// console.log(media)
+
+
+//parseFloat => Transforma o que está entre () em número com vigula / número frasionado
+let pronduto1 = parseFloat(prompt("Digite o valor do produto: "))
+
+let produto2 = parseFloat(prompt("Digite o valor do segundo produto: "))
+
+let soma = pronduto1 + produto2;
+
+console.log(soma)
+
+
